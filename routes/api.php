d267b598b22fd726796c3149d4ee07bd3375288e
Christian Busch <kontakt@christian-busch.com>
2018-10-04 15:27:50 +0200

    cleaned up some routes
    Signed-off-by: Christian Busch <kontakt@christian-busch.com>

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 //get all blocks
 Route::get('blocks', 'BlockController@showAll');
 //get specific block
