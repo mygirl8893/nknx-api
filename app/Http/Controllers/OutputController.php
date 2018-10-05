2e666b76ae767dde2eb7afca20a06acd2733b696
Christian Busch <kontakt@christian-busch.com>
2018-10-05 19:52:13 +0200

    added miner ranking option to output route
    Signed-off-by: Christian Busch <kontakt@christian-busch.com>

--- app/Http/Controllers/OutputController.php
+++ app/Http/Controllers/OutputController.php
@@ -25,45 +25,60 @@ class OutputController extends Controller
      * @return Response
      */
     public function showAll(Request $request){
+        $by_miner = $request->get('by_miner',false);
         $latest = $request->get('latest');
         $txType = $request->get('txType');
         $address = $request->get('address');
         $paginate = $request->get('per_page',50);
 
-        //hardcap
-        if(!$latest || $latest > 1000000){
-            $latest = 1000000;
-        };
-       
-        $paginate = $request->get('per_page',50);
-        $withoutPayload = $request->get('withoutpayload',false);
- 
-        $outputs_query = output::query()->orderBy('id', 'desc');
-        $outputs_query->when($latest, function ($q, $latest) { 
-            return $q->limit($latest);
-        });
-        $outputs_query->when($txType, function ($q, $txType) { 
-            return $q->whereHas('transaction', function($q) use ($txType){
-                $q->where('txType', $txType);
-            });
-            return $q->where("txType","=",$txType);
-        });
-        $outputs_query->when($address, function ($q, $address) {        
-                return $q->where('address', $address);
-        });
-
-
-        
-
-
-        if($latest > $paginate){
+        if($by_miner){
+            $outputs_query = output::select("address",DB::raw('count(*) as total'))
+                ->whereHas('transaction', function($q) use ($txType){
+                    $q->where('txType', 0);
+                })
+                ->groupBy('address')
+                ->orderBy('total','desc');
             $outputs = $outputs_query
-                ->simplePaginate($paginate);
+                    ->get();
+            return response()->json($outputs);
         }
         else{
-            $outputs = $outputs_query
-                ->get();
+
+            //hardcap
+            if(!$latest || $latest > 1000000){
+                $latest = 1000000;
+            };
+        
+            $paginate = $request->get('per_page',50);
+            $withoutPayload = $request->get('withoutpayload',false);
+    
+            $outputs_query = output::query()->orderBy('id', 'desc');
+            $outputs_query->when($latest, function ($q, $latest) { 
+                return $q->limit($latest);
+            });
+            $outputs_query->when($txType, function ($q, $txType) { 
+                return $q->whereHas('transaction', function($q) use ($txType){
+                    $q->where('txType', $txType);
+                });
+                return $q->where("txType","=",$txType);
+            });
+            $outputs_query->when($address, function ($q, $address) {        
+                    return $q->where('address', $address);
+            });
+
+
+            
+
+
+            if($latest > $paginate){
+                $outputs = $outputs_query
+                    ->simplePaginate($paginate);
+            }
+            else{
+                $outputs = $outputs_query
+                    ->get();
+            }
+            return response()->json($outputs);
         }
-        return response()->json($outputs);
     }
 }
