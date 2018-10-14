9cff983e954e521c79871e39db21434305845096
Christian Busch <kontakt@christian-busch.com>
2018-10-14 18:34:22 +0200

    login and logout routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,8 @@ use App\User;
 use App\VerifyUser;
 use App\Mail\VerifyMail;
 use Mail;
+use JWTAuth;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -32,6 +34,37 @@ class AuthController extends Controller
         ], 200);
     }
 
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if ( ! $token = JWTAuth::attempt($credentials)) {
+                return response([
+                    'status' => 'error',
+                    'error' => 'invalid.credentials',
+                    'msg' => 'Invalid Credentials.'
+                ], 400);
+        }
+        return response([
+                'status' => 'success'
+            ])
+            ->header('Authorization', $token);
+    }
+
+    public function user(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+        return response([
+                'status' => 'success',
+                'data' => $user
+            ]);
+    }
+    public function refresh()
+    {
+        return response([
+                'status' => 'success'
+            ]);
+    }
+
     public function verifyUser($token)
     {
         $verifyUser = VerifyUser::where('token', $token)->first();
@@ -49,5 +82,14 @@ class AuthController extends Controller
         }
 
         return redirect('/login')->with('status', $status);
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate();
+        return response([
+                'status' => 'success',
+                'msg' => 'Logged out Successfully.'
+            ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -36,3 +36,12 @@ Route::get('outputs', 'OutputController@showAll');
 
 Route::post('auth/register', 'AuthController@register');
 Route::get('auth/verify/{token}', 'AuthController@verifyUser');
+
+Route::post('auth/login', 'AuthController@login');
+Route::group(['middleware' => 'jwt.auth'], function(){
+  Route::get('auth/user', 'AuthController@user');
+  Route::post('auth/logout', 'AuthController@logout');
+});
+Route::group(['middleware' => 'jwt.refresh'], function(){
+  Route::get('auth/refresh', 'AuthController@refresh');
+});
