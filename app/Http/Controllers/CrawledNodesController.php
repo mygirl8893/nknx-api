f249e1222a48724a784ae17654353b72cdcfc9e7
Christian Busch <kontakt@christian-busch.com>
2018-10-22 16:27:52 +0200

    query nodelist with location

--- app/Http/Controllers/CrawledNodesController.php
+++ app/Http/Controllers/CrawledNodesController.php
@@ -10,8 +10,15 @@ use App\CrawledNode;
 
 class CrawledNodesController extends Controller
 {
-    public function showAll(){
-        $nodes = CrawledNode::all()->pluck('ip');
+    public function showAll(Request $request){
+        $withLocation = $request->get('withLocation', false);
+        if($withLocation){
+            $nodes = CrawledNode::all();
+        }
+        else{
+            $nodes = CrawledNode::all()->pluck('ip');
+        }
+       
         return response()->json($nodes);
     }
 
