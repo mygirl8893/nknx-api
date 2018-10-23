29d32d977cd0bfdcdb3163226b032edb98452c0f
Christian Busch <kontakt@christian-busch.com>
2018-10-23 19:12:03 +0200

    added nameservice columns to payload table

--- app/Payload.php
+++ app/Payload.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payload extends Model
 {
-    protected $fillable = ['sigChain','submitter'];
+    protected $fillable = ['sigChain','submitter','name','registrant'];
     public function transaction()
     {
     	return $this->belongsTo('App\Transaction');
---
+++ database/migrations/2018_10_23_170249_add_nameservice_columns_to_payloads_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNameserviceColumnsToPayloadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payloads', function (Blueprint $table) {
+            $table->text('sigchain')->nullable()->change();
+            $table->string('submitter')->nullable()->change();
+            $table->string('name')->nullable()->index();
+            $table->string('registrant')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payloads', function (Blueprint $table) {
+            $table->text('sigchain')->nullable(false)->change();
+            $table->string('submitter')->nullable(false)->change();
+            $table->dropColumn('name');
+            $table->dropColumn('registrant');
+            //
+        });
+    }
+}
