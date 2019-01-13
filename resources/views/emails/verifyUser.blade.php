ae2aca7a6fb770f733654cd7d472c9f2b547169f
Christian Busch <kontakt@christian-busch.com>
2019-01-13 13:28:24 +0100

    changed copyright date in mails
    Signed-off-by: Christian Busch <kontakt@christian-busch.com>

--- resources/views/emails/verifyUser.blade.php
+++ resources/views/emails/verifyUser.blade.php
@@ -35,7 +35,7 @@
         </div>
         <div style="margin-top: 20px; padding-top: 20px; border-top: 1px solid rgba(0,0,0,0.05);">
           <div style="color: #A5A5A5; font-size: 10px;">
-            Copyright 2018 nknX. All rights reserved.
+            Copyright 2019 nknX. All rights reserved.
           </div>
         </div>
       </div>
