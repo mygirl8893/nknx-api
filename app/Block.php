986683df741a572938d767511a9098cd8e72327b
Christian Busch <kontakt@christian-busch.com>
2019-02-09 11:57:06 +0100

    added chordID to block table
    Signed-off-by: Christian Busch <kontakt@christian-busch.com>

--- app/Block.php
+++ app/Block.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 class Block extends Model
 {
-    protected $fillable = ['hash','height','prevBlockHash','nextBlockHash','signature','signer','timestamp','transactionsRoot','version','winningHash','winningHashType','code','parameter','transaction_count'];
+    protected $fillable = ['hash','height','prevBlockHash','nextBlockHash','signature','signer','timestamp','transactionsRoot','version','winningHash','winningHashType','code','parameter','transaction_count','chordID'];
 
     public function setTimestampAttribute($value)
     {
@@ -24,5 +24,5 @@ class Block extends Model
              // do the rest of the cleanup...
         });
     }
-    
+
 }
