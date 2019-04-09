aa09c3ae1b656e25b0b36378f31c5a40177ee108
Busch <Christian.Busch@kuka.com>
2019-04-09 08:50:05 +0200

    only jobcrawler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -94,7 +94,7 @@ class Kernel extends ConsoleKernel
             }
         })->everyMinute()->name('SyncWithBlockchain')->withoutOverlapping();
 
-        $schedule->call(function () {
+        /*$schedule->call(function () {
             $nodes= Node::get();
             foreach ($nodes as $node) {
                 UpdateNode::dispatch($node->id);
@@ -273,7 +273,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             CleanUpCachedNodes::dispatch()->onQueue('maintenance');
-        })->monthly()->name('CleanUpCachedNodes')->withoutOverlapping();
+        })->monthly()->name('CleanUpCachedNodes')->withoutOverlapping();*/
     }
 
     /**
